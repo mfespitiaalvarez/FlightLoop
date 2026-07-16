695a5a0a85906e6fda6a6c712326932ac33e2bb9
mfespitiaalvarez <mfespitiaalvarez@gmail.com>
2026-07-15 21:14:27 -0700

    Header for RK4 Integrator class

---
+++ include/flightloop/integrator/rk4_integrator.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2026 mfespitiaalvarez
+//
+// SPDX-License-Identifier: MIT
+
+// 4th Order Runge-Kutta. Refer to
+// https://lpsa.swarthmore.edu/NumInt/NumIntFourth.html if you are more
+// interested in reading.
+
+#ifndef FLIGHTLOOP_INTEGRATOR_RK4_H_
+#define FLIGHTLOOP_INTEGRATOR_RK4_H_
+
+#include <Eigen/Core>
+
+#include "flightloop/integrator/integrator.h"
+
+namespace flightloop {
+
+class Rk4Integrator final : public Integrator {
+ public:
+  explicit Rk4Integrator(Eigen::Index n);
+  StepStatus Step(double t, double dt, const RhsFunction& rhs,
+                  Eigen::VectorXd& x) override;
+
+  void Reset() override {}
+
+ private:
+  Eigen::VectorXd k1_;
+  Eigen::VectorXd k2_;
+  Eigen::VectorXd k3_;
+  Eigen::VectorXd k4_;
+  Eigen::VectorXd x_stage_;
+};
+
+}  // namespace flightloop
+
+#endif  // FLIGHTLOOP_INTEGRATOR_RK4_H_
